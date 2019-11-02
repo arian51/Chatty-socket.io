64a86ad39f029f5922795c73b9f5c02798785e90
Arian51 <ariankheibari@gmail.com>
2019-11-02 16:02:43 +0330

    upload getting complicated!

--- app.js
+++ app.js
@@ -45,21 +45,21 @@ mongo.connect(mongoUrl, {
 
 		// Set socket id
 
-		 var userNames = {};
-		 socket.on('setSocketId', function (data) {
-		 	var userName = data.myUserName;
-		 	var userId = data.userId;
-		 	userNames[userName] = userId;
-		 });
+		var userNames = {};
+		socket.on('setSocketId', function (data) {
+			var userName = data.myUserName;
+			var userId = data.userId;
+			userNames[userName] = userId;
+		});
 
-		 // Private Chat 
+		// Private Chat 
 
-		 socket.on('privateChat', function (data) {
-			 user = data.usernsame;
-			 message = data.message;
+		socket.on('privateChat', function (data) {
+			user = data.usernsame;
+			message = data.message;
 			console.log(data)
 			io.to(`${userNames[user]}`).emit('privateChat', message);
-		 })
+		})
 
 		// Get chat from mongo collection
 		Chat.find().limit(100).sort({ _id: 1 }).toArray(function (err, res) {
@@ -84,7 +84,7 @@ mongo.connect(mongoUrl, {
 			let name = data.handle
 			let message = data.message
 			let room = data.room
-
+			
 			console.log(data);
 			if (name != '' && message != '' && room != '') {
 				io.sockets.emit('publicChat', data);
@@ -96,6 +96,10 @@ mongo.connect(mongoUrl, {
 		siofuServer.on("saved", function (event) {
 			console.log(event.file);
 			event.file.clientDetail.base = event.file.base;
+			console.log(event.file.pathName);
+			var fileAddress = event.file.pathName;
+			uploadFile(fileAddress);
+			
 		});
 		siofuServer.on("error", function (data) {
 			console.log("Error: " + data.memo);
@@ -112,18 +116,45 @@ mongo.connect(mongoUrl, {
 		siofuServer.listen(socket);
 	});
 
-	var options = { method: 'POST',
-    url: 'http://api.parsaspace.com/v1/files/upload',
-    headers:
-    {    
-     'content-type': 'multipart/form-data; boundary=---011000010111000001101001',
-      authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWVfbmFtZSI6ImFyaWFua2hlaWJhcmlAZ21haWwuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy91c2VyZGF0YSI6IjMxMjU4IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMiIsImlzcyI6Imh0dHA6Ly9hcGkucGFyc2FzcGFjZS5jb20vIiwiYXVkIjoiQW55IiwiZXhwIjoxNjA0MTc5MzE2LCJuYmYiOjE1NzI2NDMzMTZ9.0WfsPuI5CztCx1IpUoMqCVaorXosNFTuvcJlWpenvvI'},
-      formData:{ file: { value: fs.createReadStream('C:/Users/Arian/Desktop/photo_2019-10-07_18-37-35.jpg') ,options:{ filename: 'photo_2019-10-07_18-37-35.jpg',contentType: null } }, domain: 'myprivatesubdomain.parsaspace.com', path: '/' } };
+	// var options = { method: 'POST',
+	// url: 'http://api.parsaspace.com/v1/files/upload',
+	// headers:
+	// {    
+	//  'content-type': 'multipart/form-data; boundary=---011000010111000001101001',
+	//   authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWVfbmFtZSI6ImFyaWFua2hlaWJhcmlAZ21haWwuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy91c2VyZGF0YSI6IjMxMjU4IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMiIsImlzcyI6Imh0dHA6Ly9hcGkucGFyc2FzcGFjZS5jb20vIiwiYXVkIjoiQW55IiwiZXhwIjoxNjA0MTc5MzE2LCJuYmYiOjE1NzI2NDMzMTZ9.0WfsPuI5CztCx1IpUoMqCVaorXosNFTuvcJlWpenvvI'},
+	//   formData:{ file: { value: fs.createReadStream('C:/Users/Arian/Desktop/photo_2019-10-07_18-37-35.jpg') ,options:{ filename: 'photo_2019-10-07_18-37-35.jpg',contentType: null } }, domain: 'myprivatesubdomain.parsaspace.com', path: '/' } };
 
-      request(options, function (error, response, body) {
-      if (error) throw new Error(error);
+	//   request(options, function (error, response, body) {
+	//   if (error) throw new Error(error);
 
-      console.log(body);
-	   })
-	
+	//   console.log(body);
+	//    })
+
 })
+
+function uploadFile(fileAddress) {
+
+	var fileAddress2 = fileAddress.replace(/\\/g, "/");
+
+	var str = fileAddress2;
+	var n = str.lastIndexOf('/');
+	var fileName = str.substring(n + 1);
+	
+	console.log(fileAddress2)
+	var options = {
+		method: 'POST',
+		url: 'http://api.parsaspace.com/v1/files/upload',
+		headers:
+		{
+			'content-type': 'multipart/form-data; boundary=---011000010111000001101001',
+			authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWVfbmFtZSI6ImFyaWFua2hlaWJhcmlAZ21haWwuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy91c2VyZGF0YSI6IjMxMjU4IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMiIsImlzcyI6Imh0dHA6Ly9hcGkucGFyc2FzcGFjZS5jb20vIiwiYXVkIjoiQW55IiwiZXhwIjoxNjA0MTc5MzE2LCJuYmYiOjE1NzI2NDMzMTZ9.0WfsPuI5CztCx1IpUoMqCVaorXosNFTuvcJlWpenvvI'
+		},
+		formData: { file: { value: fs.createReadStream(fileAddress2), options: { filename: fileName, contentType: null } }, domain: 'myprivatesubdomain.parsaspace.com', path: '/' }
+	};
+
+	request(options, function (error, response, body) {
+		if (error) throw new Error(error);
+
+		console.log(body);
+	})
+}
